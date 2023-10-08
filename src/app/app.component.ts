33f787189658f7699905b4d23031a9ffab49d039
rksl64 <iferreirafernandez@safareyes.es>
2023-10-08 19:10:10 +0200

    Ismael - Footer completo, inicio mitad y carousel falta responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Nise_frontend';
+  nav!: HTMLElement;
+  offsetTop!: number;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {
+    this.nav = this.elementRef.nativeElement.querySelector('.tabs-container');
+    this.offsetTop = this.nav.offsetTop;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: any) {
+    if (window.scrollY >= this.offsetTop) {
+      this.nav.classList.add('sticky');
+    } else {
+      this.nav.classList.remove('sticky');
+    }
+  }
+
 }
+
+
