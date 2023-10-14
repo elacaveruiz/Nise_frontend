c73fb2bc70bbcdfaac425b9abf2abb356b034a02
rksl64 <iferreirafernandez@safareyes.es>
2023-10-14 13:29:39 +0200

    cambio de paginas arreglado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { AdopcionComponent } from './adopcion/adopcion.component';
+import { InicioComponent } from './inicio/inicio.component';
+const routes: Routes = [
+  {path: '', redirectTo: '/Inicio',pathMatch: 'full'},
+  {path: 'Inicio', component: InicioComponent},
+  {path: 'Adopcion', component: AdopcionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
