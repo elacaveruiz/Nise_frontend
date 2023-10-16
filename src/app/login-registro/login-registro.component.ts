d119671f72f291edac34646b727d1f07bd8f9a21
Sara <sprietourban@safareyes.es>
2023-10-16 18:50:37 +0200

    patata

--- src/app/login-registro/login-registro.component.ts
+++ src/app/login-registro/login-registro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login-registro',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-registro.component.css']
 })
 export class LoginRegistroComponent {
+
+  @ViewChild('login') login!: ElementRef;
+  @ViewChild('signup') signup!: ElementRef;
+  @ViewChild('switch') switch!: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.login.nativeElement.addEventListener('click', () => {
+      this.switch.nativeElement.classList.remove('toRight');
+      this.switch.nativeElement.classList.add('toLeft');
+    });
+
+    this.signup.nativeElement.addEventListener('click', () => {
+      this.switch.nativeElement.classList.remove('toLeft');
+      this.switch.nativeElement.classList.add('toRight');
+    });
+  }
 
 }
