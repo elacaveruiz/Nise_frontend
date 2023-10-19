69791f4fda87443846c099934c7806ab976dc930
Nico SAFA <nriberaespindola@safareyes.es>
2023-10-19 15:26:05 +0200

    pag adopcion y categorias animales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { VeterinariosComponent } from './veterinarios/veterinarios.component';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 import { ContactanosComponent } from './contactanos/contactanos.component';
 import { LoginRegistroComponent } from './login-registro/login-registro.component';
+import { MostrarMascotasComponent } from './mostrar-mascotas/mostrar-mascotas.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/Inicio',pathMatch: 'full'},
@@ -15,6 +16,7 @@ const routes: Routes = [
   {path: 'Quienes_somos', component: QuienesSomosComponent},
   {path: 'Contacta', component: ContactanosComponent},
   {path: 'Login', component: LoginRegistroComponent},
+  {path: 'MostrarMascotas', component: MostrarMascotasComponent},
   
 ];
 
