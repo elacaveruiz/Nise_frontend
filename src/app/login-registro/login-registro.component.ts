781d45698b6f82265917d9026f17383fde73b9f5
Sara <sprietourban@safareyes.es>
2023-11-03 18:34:36 +0100

    Diseño 1 de tablas crud

--- src/app/login-registro/login-registro.component.ts
+++ src/app/login-registro/login-registro.component.ts
@@ -7,22 +7,6 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 })
 export class LoginRegistroComponent {
 
-  @ViewChild('login') login!: ElementRef;
-  @ViewChild('signup') signup!: ElementRef;
-  @ViewChild('switch') switch!: ElementRef;
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.login.nativeElement.addEventListener('click', () => {
-      this.switch.nativeElement.classList.remove('toRight');
-      this.switch.nativeElement.classList.add('toLeft');
-    });
-
-    this.signup.nativeElement.addEventListener('click', () => {
-      this.switch.nativeElement.classList.remove('toLeft');
-      this.switch.nativeElement.classList.add('toRight');
-    });
-  }
 
 }
