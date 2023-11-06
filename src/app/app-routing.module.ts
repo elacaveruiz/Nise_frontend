310acc971e6bb4121d0395a4a8cc8bd63cafc336
rksl64 <iferreirafernandez@safareyes.es>
2023-11-06 18:55:27 +0100

    Ismael - Cambio paths y cambio una ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { AdopcionComponent } from './adopcion/adopcion.component';
-// import { VeterinariosComponent } from './veterinarios/veterinarios.component';
+import { VeterinariosComponent } from './veterinarios/veterinarios.component';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 import { ContactanosComponent } from './contactanos/contactanos.component';
 import { LoginRegistroComponent } from './login-registro/login-registro.component';
@@ -18,8 +18,8 @@ const routes: Routes = [
   {path: '', redirectTo: '/Inicio',pathMatch: 'full'},
   {path: 'Inicio', component: InicioComponent},
   {path: 'Adopcion', component: AdopcionComponent},
-  // {path: 'Veterinario', component: VeterinariosComponent}, //lo comento porq se duplico con el module nuevo q acabo d crear
-  {path: 'Quienes_somos', component: QuienesSomosComponent},
+  {path: 'Veterinario', component: VeterinariosComponent}, //lo comento porq se duplico con el module nuevo q acabo d crear
+  {path: 'Ayudanos', component: QuienesSomosComponent},
   {path: 'Contacta', component: ContactanosComponent},
   {path: 'Login', component: LoginRegistroComponent},
   {path: 'MostrarMascotas', component: MostrarMascotasComponent},
