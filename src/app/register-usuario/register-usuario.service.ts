64bd8703ebba091ece48c52f5b5a12c842c6b326
rksl64 <iferreirafernandez@safareyes.es>
2023-11-10 20:19:33 +0100

    Ismael - redireccion tras registros y arreglo de formulario de usuario.

--- src/app/register-usuario/register-usuario.service.ts
+++ src/app/register-usuario/register-usuario.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisterUsuario } from './register-usuario';
-
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,13 +9,14 @@ export class RegisterUsuarioService {
 
   private registerUsuarioURL: string;
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient, private router : Router) { 
     this.registerUsuarioURL= 'http://localhost:8080/register_usuario';
   }
 
 
   //CREAR
   public save(registerUsuario: RegisterUsuario){
+    this.router.navigate(['/Inicio']);
     return this.http.post<RegisterUsuario>(this.registerUsuarioURL, registerUsuario);
   }
 
