30e9bcf7b435b8ba5cf7c46d26f787cc06dcdc71
Nico SAFA <nriberaespindola@safareyes.es>
2023-11-17 18:25:04 +0100

    Nico - usuario component

---
+++ src/app/usuarios-module/usuarios-detail/usuarios-detail.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsuariosDetailComponent } from './usuarios-detail.component';
+
+describe('UsuariosDetailComponent', () => {
+  let component: UsuariosDetailComponent;
+  let fixture: ComponentFixture<UsuariosDetailComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [UsuariosDetailComponent]
+    });
+    fixture = TestBed.createComponent(UsuariosDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
